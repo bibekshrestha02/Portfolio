6f5463f436329112e22118b77e25efaf5da8bcbb
Bibek Shrestha <shresthabbks@gmail.com>
2020-03-28 18:21:25 +0545

    afteerrr

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
   },
 });
+app.use(express.static("client/build"));
 
 app.post("/contact", (req, res) => {
   const data = {
@@ -38,11 +39,9 @@ app.post("/contact", (req, res) => {
   });
 });
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log("server started"));
